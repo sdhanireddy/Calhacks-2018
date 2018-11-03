1804357e1e21f268ae8d83948c59520c6e97fc00
Kevin Tang <kevin@Kevins-MacBook-Pro-2.local>
2018-11-03 00:34:26 -0700

    borders detected

--- project.py
+++ project.py
@@ -2,10 +2,111 @@ GOOGLE_APPLICATION_CREDITIALS = './My First Project-cc40bf076871.json'
 
 import io
 import os
+import argparse
+from enum import Enum
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
+from PIL import Image, ImageDraw
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+class FeatureType(Enum):
+    PAGE = 1
+    BLOCK = 2
+    PARA = 3
+    WORD = 4
+    SYMBOL = 5
+
+
+def draw_boxes(image, bounds, color):
+    """Draw a border around the image using the hints in the vector list."""
+    draw = ImageDraw.Draw(image)
+
+    for bound in bounds:
+        draw.polygon([
+            bound.vertices[0].x, bound.vertices[0].y,
+            bound.vertices[1].x, bound.vertices[1].y,
+            bound.vertices[2].x, bound.vertices[2].y,
+            bound.vertices[3].x, bound.vertices[3].y], None, color)
+    return image
+
+
+def get_document_bounds(image_file, feature):
+    """Returns document bounds given an image."""
+    client = vision.ImageAnnotatorClient()
+
+    bounds = []
+
+    with io.open(image_file, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    document = response.full_text_annotation
+
+    # Collect specified feature bounds by enumerating all document features
+    for page in document.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    for symbol in word.symbols:
+                        if (feature == FeatureType.SYMBOL):
+                            bounds.append(symbol.bounding_box)
+
+                    if (feature == FeatureType.WORD):
+                        bounds.append(word.bounding_box)
+
+                if (feature == FeatureType.PARA):
+                    bounds.append(paragraph.bounding_box)
+
+            if (feature == FeatureType.BLOCK):
+                bounds.append(block.bounding_box)
+
+        if (feature == FeatureType.PAGE):
+            bounds.append(block.bounding_box)
+
+    # The list `bounds` contains the coordinates of the bounding boxes.
+    return bounds
+
+
+def render_doc_text(filein, fileout):
+    image = Image.open(filein)
+    bounds = get_document_bounds(filein, FeatureType.PAGE)
+    draw_boxes(image, bounds, 'blue')
+    bounds = get_document_bounds(filein, FeatureType.PARA)
+    draw_boxes(image, bounds, 'red')
+    bounds = get_document_bounds(filein, FeatureType.WORD)
+    draw_boxes(image, bounds, 'yellow')
+
+    if fileout is not 0:
+        image.save(fileout)
+    else:
+        image.show()
+
+def read_text(image, borders):
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('detect_file', help='The image for text detection.')
+    parser.add_argument('-out_file', help='Optional output file', default=0)
+    args = parser.parse_args()
+
+    parser = argparse.ArgumentParser()
+    render_doc_text(args.detect_file, args.out_file)
+
+    # open the image to show users
+    img = Image.open(args.out_file)
+    img.show()
+
+    # give users the option to say yes or no to the boardered image
+    userDecision = input("Are these borders correct? (y/n)")
+    userDecision = userDecision.lower()
+    while (userDecision != 'y' and userDecision != 'n'):
+        userDecision = input("Type 'y' or 'n'")
+    if (userDecision == 'y'):
+        correctImage = args.out_file
+    elif (userDecision == 'n'):
+
+    # text detection
